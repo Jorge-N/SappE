7b5778cce3d082ac251dd51700fb1943dac8a0ad
Jorge <valentinnave@gmail.com>
2024-09-06 10:29:13 -0300

    add modal

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native'
 import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 
@@ -7,10 +7,6 @@ const Profile = () => {
     const [nameOnChange, setNameOnChange] = useState('')
     const [name, setName] = useState("Jorge")
 
-    const toggleVisible = () => {
-      setVisible(prev => !prev)
-    }
-
     return (
     <>
       <Text style={{
@@ -18,47 +14,80 @@ const Profile = () => {
           fontSize: 24
         }}
       >
-        Holis {name}
+        Holis, {name}
       </Text>
-
-      <TextInput 
-        textAlign='center'
-        style={visible ?{
+      <Modal 
+        animationType='slide'
+        visible={visible}
+        transparent={true}
+      >
+        <View style={{
           display: 'flex',
-          padding: 10,
-          backgroundColor: '#fff',
-          borderWidth: 1,
-          borderColor: 'grey',
-        } 
-        : {
-          display: 'none'
-        }} 
-        placeholder= 'Cambiame! jeje' 
-        placeholderTextColor= 'grey'
-        onChangeText={(text) => setNameOnChange(text)}
-        value = {nameOnChange}
-      />
-      
-      <Pressable 
-        style={visible ? {display: 'flex'} : {display: 'none'}} 
-        onPress={() => {
-          nameOnChange == '' ? setName(name) : setName(nameOnChange)
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
         }}>
-        <Text
-          style={{
-            padding: 10,
-            backgroundColor: '#1D3D47',
-            color: 'white',
-            borderRadius: 20
-          }}
-        >
-          Guardar Cambios
+        <View style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 20,
+          width: 500,
+          height: 500,
+          borderRadius: 20,
+          shadowColor: 'grey',
+          shadowOffset: {width: 1, height: 1},
+          shadowRadius: 10,
+          shadowOpacity: 0.5,
+          backgroundColor: 'white',
+        }}>
+        <Text style={{
+          fontSize: 30,
+        }}>
+          Cambiar Nombre
         </Text>
-      </Pressable>
-      
-      <Pressable onPress={toggleVisible}>
+        <TextInput 
+          style={visible ?{
+            display: 'flex',
+            padding: 10,
+            backgroundColor: '#fff',
+            borderWidth: 1,
+            borderColor: 'grey',
+          } 
+          :{
+            display: 'none'
+          }} 
+          maxLength={20}
+          placeholder= 'Escribe aquí...'
+          placeholderTextColor= 'grey'
+          onChangeText={(text) => setNameOnChange(text)}
+          value = {nameOnChange}
+        />
+        
+        <Pressable 
+          style={visible ? {display: 'flex', width: 'fit-content'} : {display: 'none'}} 
+          onPress={() => {
+            setVisible(!visible)
+            nameOnChange == '' ? setName(name) : setName(nameOnChange)
+            setNameOnChange('')
+          }}>
+          <Text
+            style={{
+              padding: 10,
+              backgroundColor: '#1D3D47',
+              color: 'white',
+              borderRadius: 20,
+            }}
+          >
+            Guardar Cambios
+          </Text>
+        </Pressable>
+        </View>
+        </View> 
+      </Modal>
+      <Pressable onPress={() => setVisible(!visible)}>
         <Text style={{padding: 10, backgroundColor: 'darkred', borderRadius: 20, color: 'white'}}>
-          {!visible ? "Cambiar nombre": "Ocultar"}
+          Cambiar Nombre
         </Text>
       </Pressable>
     </>
